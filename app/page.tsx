e6fa0dcda522a46f1404ff6a9c6f6a727bce7b12
jhonattan2121 <devferri22@gmail.com>
2024-10-05 09:53:23 -0300

    responsivo page

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,13 @@ import { Box, Button, Container, Flex, Grid, GridItem, Image, keyframes, Text } 
 import { useState } from 'react';
 import { dummyProducts } from './components/store/Products';
 
+// Animação de rotação
 const spin = keyframes`
   from { transform: rotate(0deg); }
   to { transform: rotate(360deg); }
 `;
 
-const flipCardStyles = {
-  perspective: "1000px",
-};
-
+// Estilos para o cartão flip
 const flipCardInnerStyles = {
   position: "relative",
   width: "100%",
@@ -19,7 +17,6 @@ const flipCardInnerStyles = {
   textAlign: "center",
   transition: "transform 0.6s",
   transformStyle: "preserve-3d",
-  willChange: "transform",
 };
 
 const flipCardFlippedStyles = {
@@ -31,7 +28,6 @@ const flipCardSideStyles = {
   width: "100%",
   height: "100%",
   backfaceVisibility: "hidden",
-  willChange: "transform",
 };
 
 const flipCardBackStyles = {
@@ -39,7 +35,6 @@ const flipCardBackStyles = {
 };
 
 export default function Home() {
-  const [products, setProducts] = useState<any[]>([]);
   const [isFlipped, setIsFlipped] = useState<boolean[]>(Array(dummyProducts.length).fill(false)); 
 
   const handleClick = (index: number) => {
@@ -49,10 +44,17 @@ export default function Home() {
   };
 
   return (
-    <Box bg="#0a0e0b" color="white">
-      <Container maxW="container.lg" py={12}>
-        <Flex align="center" justify="center" flexDirection={{ base: 'column', md: 'row' }} textAlign={{ base: 'center', md: 'left' }}>
-          <Text fontSize="2xl" fontWeight="bold" mb={{ base: 4, md: 0 }} flex={1}>
+    <Box bg="#0a0e0b" color="white" minH="100vh">
+      {/* Cabeçalho com texto e imagem */}
+      <Container maxW="container.xl" py={12}>
+        <Flex 
+          align="center" 
+          justify="center" 
+          flexDirection={{ base: 'column', md: 'row' }} 
+          textAlign={{ base: 'center', md: 'left' }} 
+          mb={8}
+        >
+          <Text fontSize={{ base: 'lg', md: '2xl' }} fontWeight="bold" mb={{ base: 4, md: 0 }} flex={1}>
             Home to only the most immutable and unprunable artworks on the Bitcoin blockchain
             <br />
             <br />
@@ -61,26 +63,24 @@ export default function Home() {
           <Image
             src="/burnHead.svg"
             alt="Nakamoto"
-            boxSize="240px"
+            boxSize={{ base: "160px", md: "240px" }}
             animation={`${spin} 4s linear infinite`}
           />
         </Flex>
 
         <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={6} py={12}>
-          <Image src="/nsi1.png" alt="First Image" />
+          <Image src="/nsi1.png" alt="First Image" objectFit="cover"  borderRadius="md" />
         </Grid>
 
-        <Text fontSize="2xl" fontWeight="bold" textAlign="center" mb={8}>
+        <Text fontSize={{ base: 'xl', md: '2xl' }} fontWeight="bold" textAlign="center" mb={8}>
           STAMP Artworks
         </Text>
 
         <Grid templateColumns={{ base: '1fr', md: 'repeat(4, 1fr)' }} gap={6} mb={8}>
           {dummyProducts.map((product, index) => {
             const imageUrl = product.imageUrls[0];
-
             const width = product.width ?? 0;
             const height = product.height ?? 0;
-
             const isLandscape = width > height;
 
             return (
@@ -88,7 +88,7 @@ export default function Home() {
                 key={product.id}
                 textAlign="center"
                 onClick={() => handleClick(index)}
-                colSpan={isLandscape ? 2 : 1}
+                colSpan={{ base: 1, md: isLandscape ? 2 : 1 }}
               >
                 <Box sx={{ ...flipCardInnerStyles, ...(isFlipped[index] && flipCardFlippedStyles) }}>
                   {!isFlipped[index] ? (
@@ -96,6 +96,7 @@ export default function Home() {
                       src={imageUrl}
                       alt={product.name}
                       border="2px solid #fff"
+                      borderRadius="md"
                       sx={{
                         width: '100%',
                         height: 'auto',
@@ -124,9 +125,6 @@ export default function Home() {
           })}
         </Grid>
 
-
-
-
         <Flex justify="center">
           <Button
             as="a"
@@ -135,14 +133,15 @@ export default function Home() {
             colorScheme="teal"
             size="lg"
             fontWeight="bold"
+            mb={12}
           >
             Submit Art
           </Button>
         </Flex>
       </Container>
 
-      <Container maxW="container.lg" py={12}>
-        <Text fontSize="2xl" fontWeight="bold" mb={8} color="lightgreen">
+      <Container maxW="container.xl" py={12}>
+        <Text fontSize={{ base: 'xl', md: '2xl' }} fontWeight="bold" mb={8} color="lightgreen">
           Whats new in STAMPS...
         </Text>
 
@@ -151,32 +150,30 @@ export default function Home() {
             <Text fontSize="lg" mb={4}>
               <strong>STAMPS Artworks showcasing in Pepe in Bali August 2024</strong>
               <br />
-              The Pepe in Bali Exhibition at the Superlative Gallery in Bali showcases Pepe art from August 11th to August 24th, 2024. It educates on Pepes cultural significance, digital art history, and engages with the Pepe and internet culture community. The event features diverse Pepe interpretations in Bali, focusing on digital art and technology, with involvement from curators, collaborators, and the Pepe community. Held in Bali, known for digital art, the exhibition highlights the intersection of art, technology, and meme culture.
+              The Pepe in Bali Exhibition at the Superlative Gallery in Bali showcases Pepe art from August 11th to August 24th, 2024. 
+              The event features diverse Pepe interpretations in Bali, focusing on digital art and technology, with involvement from curators, collaborators, and the Pepe community.
             </Text>
-            <Image src="/nft.jpeg" alt="Pepe in Bali Exhibition" mb={4} />
-            <Image src="/nft2.jpeg" alt="Pepe in Bali Exhibition" mb={4} />
-            <Image src="/nft3.jpeg" alt="Pepe in Bali Exhibition" />
+            <Image src="/nft.jpeg" alt="Pepe in Bali Exhibition" mb={4} borderRadius="md" />
+            <Image src="/nft2.jpeg" alt="Pepe in Bali Exhibition" mb={4} borderRadius="md" />
+            <Image src="/nft3.jpeg" alt="Pepe in Bali Exhibition" borderRadius="md" />
           </GridItem>
 
           <GridItem>
-            <Image src="/Pepe in Bali.png" alt="Beeple PepeFest" />
-            <Text fontSize="lg" mb={4}>
+            <Image src="/Pepe in Bali.png" alt="Beeple PepeFest" mb={4} borderRadius="md" />
+            <Text fontSize="lg">
               <strong>STAMPS invading Beeple PepeFest August 2024</strong>
               <br />
-              PepeFest at Beeple Studios was a community-led celebration of all things Pepe. A one-day event celebrating the community and culture around Matt Furies infamous 2005 creation.
+              PepeFest at Beeple Studios was a community-led celebration of all things Pepe.
             </Text>
           </GridItem>
         </Grid>
       </Container>
 
-      <Container maxW="container.lg" py={8}>
-  <Flex justify="center" align="center">
-    <Image src="/Stamp Logo.png" alt="Stamp Icon" boxSize="50px" />
-  </Flex>
-
- 
-</Container>
-
+      <Container maxW="container.xl" py={8}>
+        <Flex justify="center" align="center">
+          <Image src="/Stamp Logo.png" alt="Stamp Icon" boxSize="50px" />
+        </Flex>
+      </Container>
     </Box>
   );
 }
