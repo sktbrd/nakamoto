a98cf56d2f89677d05ba3f0df76f75857f9cac23
jhonattan2121 <devferri22@gmail.com>
2024-10-12 02:17:53 -0300

    preparations for the website

--- app/utils/formatDate.ts
+++
@@ -1,5 +0,0 @@
-export const formatDate = (date: string) => {
-    const d = new Date(date);
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
-    return d.toLocaleDateString(undefined, options);
-}
